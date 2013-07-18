b0d61daf3c4895999a1dd1dbf512a19befcbc5b3
sanorchid <zouyaoxin@gmail.com>
2013-07-18 15:44:05 +0800

    test

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@ from fabric.api import *
 def prepare_deploy():
 	# '.\' is for windows powershell, should be './' in linux local
 	local("git add -p")
-	local("git commit -m 'test commit' ")
+	local("git commit")
 	local("git push")
 
 
