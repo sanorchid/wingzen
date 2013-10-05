c3f7753389d335b94060a0b37cf3627ef28fcec4
sanorchid <zouyaoxin@gmail.com>
2013-10-05 20:57:33 +0800

    use redactor instead of tinymce in the admin's textarea; get rid of flatpages' richtextarea 2

--- zeloplus/admin.py
+++ zeloplus/admin.py
@@ -1,17 +1,6 @@
 from django.contrib import admin
 from zeloplus.models import Category, Entry
 
-from django.contrib.flatpages.models import FlatPage
-from django.contrib.flatpages.admin import FlatPageAdmin as FlatPageAdminOld
-
-class FlatPageAdmin(FlatPageAdminOld):
-    class Media:
-        js = ('/sitemedia/js/tinymce/jscripts/tiny_mce/tiny_mce.js',
-              '/sitemedia/js/textareas.js',)
-
-# We have to unregister it, and then reregister
-admin.site.unregister(FlatPage)
-admin.site.register(FlatPage, FlatPageAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['title']}
@@ -22,10 +11,10 @@ class EntryAdmin(admin.ModelAdmin):
     list_display = ('title', 'tags', 'pub_date', 'author')
     date_hierarchy = 'pub_date'
     prepopulated_fields = {'slug': ['title']}
-    class Media:
-        js = (
-            '/sitemedia/js/tinymce/jscripts/tiny_mce/tiny_mce.js',
-            '/sitemedia/js/textareas.js',
-        )
+    #class Media:
+        #js = (
+            #'/sitemedia/js/tinymce/jscripts/tiny_mce/tiny_mce.js',
+            #'/sitemedia/js/textareas.js',
+        #)
 admin.site.register(Entry, EntryAdmin)
 
