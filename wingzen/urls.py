077d242d65557392fd9c319ea072d1e272fd9c09
sanorchid <zouyaoxin@gmail.com>
2014-07-28 10:22:56 +0800

    ....

--- wingzen/urls.py
+++ wingzen/urls.py
@@ -24,7 +24,7 @@ urlpatterns = patterns('',
     url(r'^redactor/', include('redactor.urls')),
     url(r'^oa/', include('wingoa.urls')),
     url(r'^calendar/', include('django_bootstrap_calendar.urls')),
-    url(r'^forum/', include('lbforum.urls')),
+    #url(r'^forum/', include('lbforum.urls')),
     url(r'^weixin/', 'wingzen.views.validate'),
 ) + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
